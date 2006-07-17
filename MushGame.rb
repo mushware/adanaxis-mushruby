57fb6241a6edbfc2a947699a72ae153cb8f20407
Andy Southgate <andy@mushware.com>
2006-07-17 09:01:04 +0000

    Documentation updates

---
+++ MushGame.rb
@@ -0,0 +1,169 @@
+
+class MushGame << MushObject
+# Class: MushGame
+#
+# Description:
+#
+# This object supplies a number of utility functions used to control the underlying
+# game.
+#
+# Method: cKeySymbolToName
+#
+# Provide the name string corresponding to a key value.  
+#
+# Parameters:
+#
+# key value - Key value - one of the key values from MushKeys.
+#
+# Returns:
+#
+# String containing a name for the key.
+#
+# Method: cGameModeEnter
+#
+# Exits menu mode and enters game mode.  Used to resume the game after using the menu.
+#
+# Parameters:
+#
+# None.
+#
+# Returns:
+#
+# None.
+#
+# Method: cNewGameEnter
+#
+# Exits menu mode and enters game mode, discarding the current game and creating a new one.
+#
+# Parameters:
+#
+# None.
+#
+# Returns:
+#
+# None.
+#
+# Method: cQuit
+#
+# Quits the application.
+#
+# Parameters:
+#
+# None.
+#
+# Returns:
+#
+# None.
+#
+# Method: cAxisKeySymbol
+#
+# Parameters:
+#
+# axis number - Axis key number.
+#
+# Returns:
+#
+# Key symbol value (a MushKeys constant) configured for that axis.
+#
+# Method: cAxisKeySet
+#
+# Sets the configured key for an axis.
+#
+# Parameters:
+#
+# key value - Key symbol value.
+# axis number - Axis key number.
+#
+# Returns:
+#
+# None.
+#
+# Method: cAxisSymbol
+#
+# Returns the configured control axis (e.g. joystick x) for a player axis (e.g. left/right).
+#
+# Parameters:
+#
+# axis number - Axis number.
+#
+# Returns:
+#
+# Axis symbol value configured for the supplied axis number.
+#
+# Method: cAxisSet
+#
+# Sets the configured axis for an axis number.
+#
+# Parameters:
+#
+# axis symbol - Axis symbol value.
+# axis number - Axis number.
+#
+# Returns:
+#
+# None.
+#
+# Method: cAxisName
+#
+# Returns the name for an axis symbol.
+#
+# Parameters:
+#
+# axis symbol - Axis symbol value.
+#
+# Returns:
+#
+# Name string for that axis.
+#
+# Method: cKeySymbol
+#
+# Returns the configured key value for a control key.
+#
+# Parameters:
+#
+# key number - Key number.
+#
+# Returns:
+#
+# Key symbol value configured for the supplied key number.
+#
+# Method: cKeySet
+#
+# Sets the configured key for a key control.
+#
+# Parameters:
+#
+# key symbol - Key symbol value.
+# key number - Key number.
+#
+# Returns:
+#
+# None.
+#
+# Method: cNumJoysticks
+#
+# Returns the number of joysticks connected to the system.
+#
+# Parameters:
+#
+# None.
+#
+# Returns:
+#
+# Number of joysticks.
+#
+# Method: cControlsToDefaultSet
+#
+# Reloads the default control configuration.
+#
+# Parameters:
+#
+# None.
+#
+# Returns:
+#
+# None.
+#
+# Group: Links
+#- Wrapper file:doxygen/class_mush_game_ruby.html
+end
--- MushRotation.rb
+++ MushRotation.rb
@@ -1,4 +1,74 @@
 
-# Other methods provided by MushMeshRubyTools.cpp
 class MushRotation
+# Class: MushRotation
+#
+# Description:
+#
+# This object contains a 4D rotation, which can be used to rotate vectors or other rotations.
+#
+# Method: new
+#
+# Creates a new MushRotation object.
+#
+# Parameters:
+#
+# None.
+#
+# Returns:
+#
+# New MushRotation object.
+#
+# Default:
+#
+# The MushRotation object returned by new is the identity i.e. provides zero rotation.
+# Non-zero rotations can be obtained from the <MushTools> object.
+#
+# Example:
+#
+# (example)
+# rotation1 = MushTools.cRotationInXYPlane(Math::PI/2)
+# (end)
+#
+# Method: mRotate
+#
+# Applies the rotation to another object.
+#
+# Returns:
+#
+# None.
+#
+# Paramaters:
+#
+# This method takes either a vector or another rotation as a parameter.  If the parameter
+# is a <MushVector>, it rotates the vector.  If another <MushRotation>, the parameter
+# is set to a combination of the original rotation followed by this rotation.
+#
+# Example:
+#
+# (example)
+# vec1 = MushVector.new(1,0,0,0)
+# rot1 = MushTools.cRotationInXYPlane(Math::PI/2)
+# rot1.mRotate(vec1)
+# # vec1 is now (0,1,0,0)
+#
+# vec1 = MushVector.new(1,0,0,0)
+# rot1 = MushTools.cRotationInXYPlane(Math::PI/2)
+# rot2 = MushTools.cRotationInYZPlane(Math::PI/2)
+# rot2.mRotate(rot1)
+# # rot1 is now a rotation that applies the original rot1 then rot2
+# rot1.mRotate(vec1)
+# # vec1 is now (0,0,1,0)
+# (end)
+#
+# Method: ==
+#
+# Tests whether two rotations are equal.
+#
+# Returns:
+#
+# Boolean.
+#
+# Group: Links
+#- Wrapper file:doxygen/class_mush_mesh_ruby_rotation.html
+#- Implemetation file:doxygen/class_mush_mesh_quaternion_pair.html
 end
