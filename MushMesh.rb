4c5a3e98ea80b01039cd0154637e476eabd24de1
Andy Southgate <andy@mushware.com>
2006-08-01 13:41:10 +0000

    Pre-release updates

--- MushMesh.rb
+++ MushMesh.rb
@@ -1,3 +1,25 @@
+#%Header {
+##############################################################################
+#
+# File: data-adanaxis/mushruby/MushMesh.rb
+#
+# Author: Andy Southgate 2006
+#
+# This file contains original work by Andy Southgate.  The author and his
+# employer (Mushware Limited) irrevocably waive all of their copyright rights
+# vested in this particular version of this file to the furthest extent
+# permitted.  The author and Mushware Limited also irrevocably waive any and
+# all of their intellectual property rights arising from said file and its
+# creation that would otherwise restrict the rights of any party to use and/or
+# distribute the use of, the techniques and methods used herein.  A written
+# waiver can be obtained via http://www.mushware.com/.
+#
+# This software carries NO WARRANTY of any kind.
+#
+##############################################################################
+#%Header } jRhUtNxkmwSZNTs7faAzsw
+# $Id$
+# $Log$
 
 class MushMesh
 # Class: MushMesh
